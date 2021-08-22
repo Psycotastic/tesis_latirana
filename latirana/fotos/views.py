52e302728e25282c2b97d5c07d75d90d6419ee4a
yo <yo@yo.yo>
2021-08-22 02:32:21 -0400

    Formulario

--- latirana/fotos/views.py
+++ latirana/fotos/views.py
@@ -1,9 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import ImageForm
+from .models import Post
 
 
 def index(request):
-    return render(request, 'home.html')
+    post_list = Post.objects.all()
+    return render(request, 'home.html', {'post_list': post_list})
 
 def upload(request):
-    return render(request, 'upload.html')
+    # return render(request, 'upload.html')
+    """Process images uploaded by users"""
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Get the current instance object to display in the template
+            img_obj = form.instance
+            return render(request, 'upload.html', {'form': form, 'img_obj': img_obj})
+    else:
+        form = ImageForm()
+    return render(request, 'upload.html', {'form': form})
--- latirana/latirana/settings.py
+++ latirana/latirana/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fotos.apps.FotosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,9 +123,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/src/'
+# STATIC_ROOT = str(BASE_DIR.joinpath('src')) # new
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('src')),) # new
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Subir imágenes
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
